535797809d29d8258a5c8def7b25a428baef0cda
payamd <payam.d7@gmail.com>
2023-08-02 20:48:56 -0400

    add event handler to the app

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,13 +1,25 @@
+import { MouseEvent } from "react";
+
 function ListGroup() {
+  const cities = ["New York", "Tokio", "London", "Tehran"];
+
+  const handleClick = (event: MouseEvent) => console.log(event);
+
   return (
     <>
       <h1>List:</h1>
+
+      {cities.length === 0 ? (
+        <p> The list is empty, these two are the same</p>
+      ) : null}
+      {cities.length === 0 && <p> The list is empty</p>}
+
       <ul className="list-group">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
+        {cities.map((item, index) => (
+          <li className="list-group-item" key={item} onClick={handleClick}>
+            {item}
+          </li>
+        ))}
       </ul>
     </>
   );
