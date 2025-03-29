3c6298d2a5b76f06583424460cc45c489cb6f372
payamd <payamd@users.noreply.github.com>
2025-03-29 16:15:43 -0400

    making list clickable

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,13 +1,25 @@
 function ListGroup() {
+  let items = [
+    { id: 1, name: "An item" },
+    { id: 2, name: "A second item" },
+    { id: 3, name: "A third item" },
+    { id: 4, name: "A fourth item" },
+    { id: 5, name: "And a fifth one" },
+  ];
+
   return (
     <>
       <h1>List:</h1>
       <ul className="list-group">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
+        {items.map((item) => (
+          <li
+            key={item.id}
+            className="list-group-item"
+            onClick={() => console.log(item.id)}
+          >
+            {item.name}
+          </li>
+        ))}
       </ul>
     </>
   );
