0201fa882a7022e2b23f9c255abb448b04a5d305
payamd <payamd@users.noreply.github.com>
2025-03-30 00:06:42 -0400

    index selection added to the application

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,23 +1,29 @@
+import { useState } from "react";
+
 function ListGroup() {
   let items = [
-    { id: 1, name: "An item" },
-    { id: 2, name: "A second item" },
-    { id: 3, name: "A third item" },
-    { id: 4, name: "A fourth item" },
-    { id: 5, name: "And a fifth one" },
+    "An item",
+    "A second item",
+    "A third item",
+    "A fourth item",
+    "And a fifth one",
   ];
-
+  const [selectedIndex, setSelectedIndex] = useState(-1);
   return (
     <>
       <h1>List:</h1>
       <ul className="list-group">
-        {items.map((item) => (
+        {items.map((item, index) => (
           <li
-            key={item.id}
-            className="list-group-item"
-            onClick={() => console.log(item.name, item.id)}
+            key={item}
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            onClick={() => console.log(item, setSelectedIndex(index))}
           >
-            {item.name}
+            {item}
           </li>
         ))}
       </ul>
