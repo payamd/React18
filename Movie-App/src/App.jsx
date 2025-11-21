4b001d1e554a18b9d0407e8acfba55accb0b65a2
payamd <payamd@users.noreply.github.com>
2025-11-21 15:12:18 -0500

    new updates 2025

---
+++ Movie-App/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const App = () =>  {
+  return (  
+<main>
+  <div className="patter"/>
+  <div className="wrapper">
+    <header>
+      <h1> find your <span className = "text-gradient">movie</span> you want to watch </h1>
+    </header>
+  </div>
+  </main>
+  )
+}
+
+export default App
